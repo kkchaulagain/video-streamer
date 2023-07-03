ef10c9a3f4490aa923ee4f09a38b2821bcad5c48
kiran chaulagain <kkchaulagain@gmail.com>
2023-07-03 18:15:51 +0545

    initialize

--- app.py
+++ app.py
@@ -1,44 +1,40 @@
+import subprocess
 from flask import Flask, Response, render_template
 import cv2
-import threading
-
-outputFrame = None
-lock = threading.Lock()
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # use 0 for web camera
 
-camera = cv2.VideoCapture(0)
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def generate():
-    global outputFrame, lock
-    while True:
-        with lock:
-            if outputFrame is None:
-                continue
-            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-            if not flag:
-                continue
-        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def read_frame():
-    global camera, outputFrame, lock
-    while True:
-        ret, frame = camera.read()
-        with lock:
-            outputFrame = frame.copy()
+@app.route('/hls_stream.m3u8')
+def hls_stream():
+    # Run FFmpeg command to convert frames to video
+    cmd = [
+        'ffmpeg', '-f', 'image2pipe', '-framerate', '30', '-i', '-',
+        '-vf', 'format=yuv420p', '-c:v', 'libx264', '-f', 'hls', '-'
+    ]
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-t = threading.Thread(target=read_frame)
-t.daemon = True
-t.start()
+    # Stream the output of FFmpeg process
+    return Response(p.stdout, mimetype='application/vnd.apple.mpegurl')
 
 if __name__ == '__main__':
-    from gunicorn.app.wsgiapp import run
-    run()
+    app.run(host='0.0.0.0', port=4000, debug=True)
